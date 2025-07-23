bf5d40261e42449763d726d63641a51b0abbefbd
Arindam Singh <118199077+SinghArindam@users.noreply.github.com>
2025-07-23 21:19:40 +0530

    Time: 20 ms (5.12%) | Memory: 17.7 MB (64.60%) - LeetSync

--- submissions/13-roman-to-integer/roman-to-integer.py
+++ submissions/13-roman-to-integer/roman-to-integer.py
@@ -1,17 +1,32 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        n = len(s)
-        i = 1
-        num = 0
-        cases = ['IV', 'IX', 'XL', 'XC', 'CD', 'CM']
-        nums = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000,
-                'IV':4, 'IX':9, 'XL':40, 'XC':90, 'CD':400, 'CM':900}
-        while i < n+1:
-            if i<n and (str(s[i-1] + s[i]) in cases):
-                num += nums[str(s[i-1] + s[i])]
-                i += 2
-                continue
-            num += nums[s[i-1]]
-            i += 1
+        # Approach 1
+        # n = len(s)
+        # i = 1
+        # num = 0
+        # cases = ['IV', 'IX', 'XL', 'XC', 'CD', 'CM']
+        # nums = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000,
+        #         'IV':4, 'IX':9, 'XL':40, 'XC':90, 'CD':400, 'CM':900}
+        # while i < n+1:
+        #     if i<n and (str(s[i-1] + s[i]) in cases):
+        #         num += nums[str(s[i-1] + s[i])]
+        #         i += 2
+        #         continue
+        #     num += nums[s[i-1]]
+        #     i += 1
+
+        # Approach 2
+        nums_single = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        nums_double = {'IV':4, 'IX':9, 'XL':40, 'XC':90, 'CD':400, 'CM':900}
+
+        for i in nums_double:
+            s = s.replace(i, str(nums_double[i])+ ' ')
+        for i in nums_single:
+            s = s.replace(i, str(nums_single[i])+ ' ')
+        # s = [int(i) for i in s.split()]
+        # num = sum(s)
+        
+        num = sum([int(i) for i in s.split()])
+        # print(s)
         return num
         
