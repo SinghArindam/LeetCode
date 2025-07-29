0871d72e139e1de662d2605ee256130e6c40ed75
Arindam Singh <118199077+SinghArindam@users.noreply.github.com>
2025-07-29 11:31:45 +0530

    Time: 7 ms (25.40%) | Memory: 17.9 MB (44.56%) - LeetSync

--- submissions/1036-rotting-oranges/rotting-oranges.py
+++ submissions/1036-rotting-oranges/rotting-oranges.py
@@ -29,62 +29,62 @@ class Solution:
         # return elapsedTime
 
         # Approach 2
-        def is_safe(i, j, n, m):
-            return 0 <= i < n and 0 <= j < m
-        def dfs(grid, i, j, time):
-            n = len(grid)
-            m = len(grid[0])
-            grid[i][j] = time
-            directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
-            for dir in directions:
-                x = i + dir[0]
-                y = j + dir[1]
-                if is_safe(x, y, n, m) and (grid[x][y] == 1 or grid[x][y] > time + 1):
-                    dfs(grid, x, y, time + 1)
-        n = len(grid)
-        m = len(grid[0])
-        elapsed_time = 0
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j] == 2:
-                    dfs(grid, i, j, 2)
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j] == 1:
-                    return -1
-                elapsed_time = max(elapsed_time, grid[i][j] - 2)
-        return elapsed_time
-
-        # Approach 3
-        # n, m = len(grid), len(grid[0])
-        # num_of_fresh = 0
-        # ls_rotten = []
+        # def is_safe(i, j, n, m):
+        #     return 0 <= i < n and 0 <= j < m
+        # def dfs(grid, i, j, time):
+        #     n = len(grid)
+        #     m = len(grid[0])
+        #     grid[i][j] = time
+        #     directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+        #     for dir in directions:
+        #         x = i + dir[0]
+        #         y = j + dir[1]
+        #         if is_safe(x, y, n, m) and (grid[x][y] == 1 or grid[x][y] > time + 1):
+        #             dfs(grid, x, y, time + 1)
+        # n = len(grid)
+        # m = len(grid[0])
+        # elapsed_time = 0
         # for i in range(n):
         #     for j in range(m):
         #         if grid[i][j] == 2:
-        #             ls_rotten.append((i, j))                    
+        #             dfs(grid, i, j, 2)
+        # for i in range(n):
+        #     for j in range(m):
         #         if grid[i][j] == 1:
-        #             num_of_fresh += 1
-        # print(f"fresh:{num_of_fresh}\nrotten:{ls_rotten}")
-        # t=0
-        # dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]]
-        # if num_of_fresh == 0:
-        #     return 0
-        # while ls_rotten and num_of_fresh>0:
-        #     num_rotten_curr_level = len(ls_rotten)
-        #     for z in range(num_rotten_curr_level):
-        #         i, j = ls_rotten.pop(0)
-        #         for dr in dirs:
-        #             x = i + dr[0]
-        #             y = j + dr[1]
-        #             if (0<=x<n and 0<=y<m) and (grid[x][y] == 1):
-        #                 grid[x][y] = 2
-        #                 num_of_fresh -= 1
-        #                 ls_rotten.append((x, y))
-        #                 print(f"time:{t}\nfresh:{num_of_fresh}\nrotten:{ls_rotten}")
-        #     t+=1
+        #             return -1
+        #         elapsed_time = max(elapsed_time, grid[i][j] - 2)
+        # return elapsed_time
 
-        # if num_of_fresh > 0:
-        #     return -1
-        # return max(0,t)
+        # Approach 3
+        n, m = len(grid), len(grid[0])
+        num_of_fresh = 0
+        ls_rotten = []
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 2:
+                    ls_rotten.append((i, j))                    
+                if grid[i][j] == 1:
+                    num_of_fresh += 1
+        print(f"fresh:{num_of_fresh}\nrotten:{ls_rotten}")
+        t=0
+        dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+        if num_of_fresh == 0:
+            return 0
+        while ls_rotten and num_of_fresh>0:
+            num_rotten_curr_level = len(ls_rotten)
+            for z in range(num_rotten_curr_level):
+                i, j = ls_rotten.pop(0)
+                for dr in dirs:
+                    x = i + dr[0]
+                    y = j + dr[1]
+                    if (0<=x<n and 0<=y<m) and (grid[x][y] == 1):
+                        grid[x][y] = 2
+                        num_of_fresh -= 1
+                        ls_rotten.append((x, y))
+                        print(f"time:{t}\nfresh:{num_of_fresh}\nrotten:{ls_rotten}")
+            t+=1
+
+        if num_of_fresh > 0:
+            return -1
+        return max(0,t)
         
