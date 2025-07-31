e03f1fa4d7f46eab93da6f12b53d4e772585ae78
Arindam Singh <118199077+SinghArindam@users.noreply.github.com>
2025-07-31 23:16:48 +0530

    Time: 7 ms (54.97%) | Memory: 21.6 MB (88.09%) - LeetSync

--- submissions/130-surrounded-regions/surrounded-regions.py
+++ submissions/130-surrounded-regions/surrounded-regions.py
@@ -1,30 +1,68 @@
+
+# Approach 1
+# class Solution:
+#     def solve(self, board: list[list[str]]) -> None:
+#         if not board or not board[0]:
+#             return
+#         num_rows = len(board)
+#         num_cols = len(board[0])
+#         def dfs_mark_safe(row, col):
+#             if not (0 <= row < num_rows and 0 <= col < num_cols and board[row][col] == 'O'):
+#                 return
+#             board[row][col] = 'S'
+#             dfs_mark_safe(row + 1, col)
+#             dfs_mark_safe(row - 1, col)
+#             dfs_mark_safe(row, col + 1)
+#             dfs_mark_safe(row, col - 1)
+#         for r_idx in range(num_rows):
+#             if board[r_idx][0] == 'O':
+#                 dfs_mark_safe(r_idx, 0)
+#             if board[r_idx][num_cols - 1] == 'O':
+#                 dfs_mark_safe(r_idx, num_cols - 1)
+#         for c_idx in range(num_cols):
+#             if board[0][c_idx] == 'O':
+#                 dfs_mark_safe(0, c_idx)
+#             if board[num_rows - 1][c_idx] == 'O':
+#                 dfs_mark_safe(num_rows - 1, c_idx)
+#         for r_idx in range(num_rows):
+#             for c_idx in range(num_cols):
+#                 if board[r_idx][c_idx] == 'O':
+#                     board[r_idx][c_idx] = 'X'
+#                 elif board[r_idx][c_idx] == 'S':
+#                     board[r_idx][c_idx] = 'O'
+
+import collections
 class Solution:
     def solve(self, board: list[list[str]]) -> None:
         if not board or not board[0]:
             return
-        num_rows = len(board)
-        num_cols = len(board[0])
-        def dfs_mark_safe(row, col):
-            if not (0 <= row < num_rows and 0 <= col < num_cols and board[row][col] == 'O'):
-                return
-            board[row][col] = 'S'
-            dfs_mark_safe(row + 1, col)
-            dfs_mark_safe(row - 1, col)
-            dfs_mark_safe(row, col + 1)
-            dfs_mark_safe(row, col - 1)
-        for r_idx in range(num_rows):
-            if board[r_idx][0] == 'O':
-                dfs_mark_safe(r_idx, 0)
-            if board[r_idx][num_cols - 1] == 'O':
-                dfs_mark_safe(r_idx, num_cols - 1)
-        for c_idx in range(num_cols):
-            if board[0][c_idx] == 'O':
-                dfs_mark_safe(0, c_idx)
-            if board[num_rows - 1][c_idx] == 'O':
-                dfs_mark_safe(num_rows - 1, c_idx)
-        for r_idx in range(num_rows):
-            for c_idx in range(num_cols):
-                if board[r_idx][c_idx] == 'O':
-                    board[r_idx][c_idx] = 'X'
-                elif board[r_idx][c_idx] == 'S':
-                    board[r_idx][c_idx] = 'O'
+        row_count = len(board)
+        col_count = len(board[0])
+        q = collections.deque()
+        for r in range(row_count):
+            if board[r][0] == 'O':
+                q.append((r, 0))
+                board[r][0] = '#'
+            if board[r][col_count - 1] == 'O':
+                q.append((r, col_count - 1))
+                board[r][col_count - 1] = '#'
+        for c in range(col_count):
+            if board[0][c] == 'O':
+                q.append((0, c))
+                board[0][c] = '#'
+            if board[row_count - 1][c] == 'O':
+                q.append((row_count - 1, c))
+                board[row_count - 1][c] = '#'
+        while q:
+            r, c = q.popleft()
+            for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                next_r, next_c = r + dr, c + dc
+                if 0 <= next_r < row_count and 0 <= next_c < col_count and board[next_r][next_c] == 'O':
+                    board[next_r][next_c] = '#'
+                    q.append((next_r, next_c))
+        for r in range(row_count):
+            for c in range(col_count):
+                if board[r][c] == 'O':
+                    board[r][c] = 'X'
+                elif board[r][c] == '#':
+                    board[r][c] = 'O'
