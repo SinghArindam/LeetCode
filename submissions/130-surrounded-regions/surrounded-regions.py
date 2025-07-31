7a40b5c189760d7f8e59bbfdb6e791938021f69d
Arindam Singh <118199077+SinghArindam@users.noreply.github.com>
2025-07-31 23:18:18 +0530

    Time: 4 ms (72.63%) | Memory: 21.8 MB (56.89%) - LeetSync

--- submissions/130-surrounded-regions/surrounded-regions.py
+++ submissions/130-surrounded-regions/surrounded-regions.py
@@ -69,36 +69,70 @@
 #                 elif board[r][c] == '#':
 #                     board[r][c] = 'O'
 
+# Approach 3
+# class Solution:
+#     def solve(self, board: list[list[str]]) -> None:
+#         if not board or not board[0]:
+#             return
+#         height = len(board)
+#         width = len(board[0])
+#         parent = {i: i for i in range(height * width + 1)}
+#         safe_node = height * width
+#         def find_set(v):
+#             if v == parent[v]:
+#                 return v
+#             parent[v] = find_set(parent[v])
+#             return parent[v]
+#         def unite_sets(a, b):
+#             a = find_set(a)
+#             b = find_set(b)
+#             if a != b:
+#                 parent[b] = a
+#         for row in range(height):
+#             for col in range(width):
+#                 if board[row][col] == 'O':
+#                     position_1d = row * width + col
+#                     if row == 0 or row == height - 1 or col == 0 or col == width - 1:
+#                         unite_sets(position_1d, safe_node)
+#                     if row + 1 < height and board[row + 1][col] == 'O':
+#                         unite_sets(position_1d, (row + 1) * width + col)
+#                     if col + 1 < width and board[row][col + 1] == 'O':
+#                         unite_sets(position_1d, row * width + (col + 1))
+#         for row in range(height):
+#             for col in range(width):
+#                 if board[row][col] == 'O':
+#                     if find_set(row * width + col) != find_set(safe_node):
+#                         board[row][col] = 'X'
+
+
+# Approach 4
 class Solution:
     def solve(self, board: list[list[str]]) -> None:
-        if not board or not board[0]:
+        if not board:
             return
-        height = len(board)
-        width = len(board[0])
-        parent = {i: i for i in range(height * width + 1)}
-        safe_node = height * width
-        def find_set(v):
-            if v == parent[v]:
-                return v
-            parent[v] = find_set(parent[v])
-            return parent[v]
-        def unite_sets(a, b):
-            a = find_set(a)
-            b = find_set(b)
-            if a != b:
-                parent[b] = a
-        for row in range(height):
-            for col in range(width):
-                if board[row][col] == 'O':
-                    position_1d = row * width + col
-                    if row == 0 or row == height - 1 or col == 0 or col == width - 1:
-                        unite_sets(position_1d, safe_node)
-                    if row + 1 < height and board[row + 1][col] == 'O':
-                        unite_sets(position_1d, (row + 1) * width + col)
-                    if col + 1 < width and board[row][col + 1] == 'O':
-                        unite_sets(position_1d, row * width + (col + 1))
-        for row in range(height):
-            for col in range(width):
-                if board[row][col] == 'O':
-                    if find_set(row * width + col) != find_set(safe_node):
-                        board[row][col] = 'X'
+        m_dim = len(board)
+        n_dim = len(board[0])
+        border_cells = []
+        for i in range(m_dim):
+            border_cells.append((i, 0))
+            border_cells.append((i, n_dim - 1))
+        for j in range(n_dim):
+            border_cells.append((0, j))
+            border_cells.append((m_dim - 1, j))
+        for row, col in border_cells:
+            if board[row][col] == 'O':
+                stack = [(row, col)]
+                board[row][col] = 'E'
+                while stack:
+                    curr_r, curr_c = stack.pop()
+                    for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                        next_r, next_c = curr_r + dr, curr_c + dc
+                        if 0 <= next_r < m_dim and 0 <= next_c < n_dim and board[next_r][next_c] == 'O':
+                            board[next_r][next_c] = 'E'
+                            stack.append((next_r, next_c))
+        for i in range(m_dim):
+            for j in range(n_dim):
+                if board[i][j] == 'O':
+                    board[i][j] = 'X'
+                elif board[i][j] == 'E':
+                    board[i][j] = 'O'
