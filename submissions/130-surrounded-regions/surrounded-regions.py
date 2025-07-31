99a2866f7ee77cdea34341efb0358256fd071ffb
Arindam Singh <118199077+SinghArindam@users.noreply.github.com>
2025-07-31 23:17:30 +0530

    Time: 57 ms (8.48%) | Memory: 24.5 MB (14.62%) - LeetSync

--- submissions/130-surrounded-regions/surrounded-regions.py
+++ submissions/130-surrounded-regions/surrounded-regions.py
@@ -31,38 +31,74 @@
 #                 elif board[r_idx][c_idx] == 'S':
 #                     board[r_idx][c_idx] = 'O'
 
-import collections
+
+# Approach 2
+# import collections
+# class Solution:
+#     def solve(self, board: list[list[str]]) -> None:
+#         if not board or not board[0]:
+#             return
+#         row_count = len(board)
+#         col_count = len(board[0])
+#         q = collections.deque()
+#         for r in range(row_count):
+#             if board[r][0] == 'O':
+#                 q.append((r, 0))
+#                 board[r][0] = '#'
+#             if board[r][col_count - 1] == 'O':
+#                 q.append((r, col_count - 1))
+#                 board[r][col_count - 1] = '#'
+#         for c in range(col_count):
+#             if board[0][c] == 'O':
+#                 q.append((0, c))
+#                 board[0][c] = '#'
+#             if board[row_count - 1][c] == 'O':
+#                 q.append((row_count - 1, c))
+#                 board[row_count - 1][c] = '#'
+#         while q:
+#             r, c = q.popleft()
+#             for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+#                 next_r, next_c = r + dr, c + dc
+#                 if 0 <= next_r < row_count and 0 <= next_c < col_count and board[next_r][next_c] == 'O':
+#                     board[next_r][next_c] = '#'
+#                     q.append((next_r, next_c))
+#         for r in range(row_count):
+#             for c in range(col_count):
+#                 if board[r][c] == 'O':
+#                     board[r][c] = 'X'
+#                 elif board[r][c] == '#':
+#                     board[r][c] = 'O'
+
 class Solution:
     def solve(self, board: list[list[str]]) -> None:
         if not board or not board[0]:
             return
-        row_count = len(board)
-        col_count = len(board[0])
-        q = collections.deque()
-        for r in range(row_count):
-            if board[r][0] == 'O':
-                q.append((r, 0))
-                board[r][0] = '#'
-            if board[r][col_count - 1] == 'O':
-                q.append((r, col_count - 1))
-                board[r][col_count - 1] = '#'
-        for c in range(col_count):
-            if board[0][c] == 'O':
-                q.append((0, c))
-                board[0][c] = '#'
-            if board[row_count - 1][c] == 'O':
-                q.append((row_count - 1, c))
-                board[row_count - 1][c] = '#'
-        while q:
-            r, c = q.popleft()
-            for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                next_r, next_c = r + dr, c + dc
-                if 0 <= next_r < row_count and 0 <= next_c < col_count and board[next_r][next_c] == 'O':
-                    board[next_r][next_c] = '#'
-                    q.append((next_r, next_c))
-        for r in range(row_count):
-            for c in range(col_count):
-                if board[r][c] == 'O':
-                    board[r][c] = 'X'
-                elif board[r][c] == '#':
-                    board[r][c] = 'O'
+        height = len(board)
+        width = len(board[0])
+        parent = {i: i for i in range(height * width + 1)}
+        safe_node = height * width
+        def find_set(v):
+            if v == parent[v]:
+                return v
+            parent[v] = find_set(parent[v])
+            return parent[v]
+        def unite_sets(a, b):
+            a = find_set(a)
+            b = find_set(b)
+            if a != b:
+                parent[b] = a
+        for row in range(height):
+            for col in range(width):
+                if board[row][col] == 'O':
+                    position_1d = row * width + col
+                    if row == 0 or row == height - 1 or col == 0 or col == width - 1:
+                        unite_sets(position_1d, safe_node)
+                    if row + 1 < height and board[row + 1][col] == 'O':
+                        unite_sets(position_1d, (row + 1) * width + col)
+                    if col + 1 < width and board[row][col + 1] == 'O':
+                        unite_sets(position_1d, row * width + (col + 1))
+        for row in range(height):
+            for col in range(width):
+                if board[row][col] == 'O':
+                    if find_set(row * width + col) != find_set(safe_node):
+                        board[row][col] = 'X'
