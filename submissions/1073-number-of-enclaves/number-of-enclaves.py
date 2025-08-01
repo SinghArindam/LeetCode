b462134c10721d30adc3827bc20250af7da15b1c
Arindam Singh <118199077+SinghArindam@users.noreply.github.com>
2025-08-01 22:50:47 +0530

    Time: 233 ms (10.86%) | Memory: 27.5 MB (64.18%) - LeetSync

---
+++ submissions/1073-number-of-enclaves/number-of-enclaves.py
@@ -0,0 +1,31 @@
+# Approach 1
+class Solution:
+    def numEnclaves(self, board: list[list[int]]) -> int:
+        num_rows = len(board)
+        if num_rows == 0:
+            return 0
+        num_cols = len(board[0])
+        visited_cells = set()
+        enclaved_land_cells = 0
+        for row_idx in range(num_rows):
+            for col_idx in range(num_cols):
+                if board[row_idx][col_idx] == 1 and (row_idx, col_idx) not in visited_cells:
+                    island_size = 0
+                    is_boundary_island = False
+                    path_stack = [(row_idx, col_idx)]
+                    visited_cells.add((row_idx, col_idx))
+                    current_island_nodes = []
+                    while path_stack:
+                        current_row, current_col = path_stack.pop()
+                        island_size += 1
+                        if current_row == 0 or current_row == num_rows - 1 or current_col == 0 or current_col == num_cols - 1:
+                            is_boundary_island = True
+                        for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                            next_row, next_col = current_row + dr, current_col + dc
+                            if 0 <= next_row < num_rows and 0 <= next_col < num_cols and \
+                               board[next_row][next_col] == 1 and (next_row, next_col) not in visited_cells:
+                                visited_cells.add((next_row, next_col))
+                                path_stack.append((next_row, next_col))
+                    if not is_boundary_island:
+                        enclaved_land_cells += island_size
+        return enclaved_land_cells
