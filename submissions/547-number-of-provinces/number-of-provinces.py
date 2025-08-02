4a937815b9e4d2c9813dba7edeb15a9af74cd282
Arindam Singh <118199077+SinghArindam@users.noreply.github.com>
2025-08-02 23:11:14 +0530

    Time: 7 ms (55.08%) | Memory: 19.1 MB (58.75%) - LeetSync

--- submissions/547-number-of-provinces/number-of-provinces.py
+++ submissions/547-number-of-provinces/number-of-provinces.py
@@ -22,6 +22,31 @@
 
 
 # Approach 2
+# import collections
+
+# class Solution:
+#     def findCircleNum(self, isConnected: list[list[int]]) -> int:
+#         num_cities = len(isConnected)
+#         visited = set()
+#         num_provinces = 0
+
+#         def dfs(city_index):
+#             for neighbor, is_linked in enumerate(isConnected[city_index]):
+#                 if is_linked and neighbor not in visited:
+#                     visited.add(neighbor)
+#                     dfs(neighbor)
+
+#         for i in range(num_cities):
+#             if i not in visited:
+#                 num_provinces += 1
+#                 visited.add(i)
+#                 dfs(i)
+        
+#         return num_provinces
+
+
+
+# Approach 3
 import collections
 
 class Solution:
@@ -30,16 +55,16 @@ class Solution:
         visited = set()
         num_provinces = 0
 
-        def dfs(city_index):
-            for neighbor, is_linked in enumerate(isConnected[city_index]):
-                if is_linked and neighbor not in visited:
-                    visited.add(neighbor)
-                    dfs(neighbor)
-
         for i in range(num_cities):
             if i not in visited:
                 num_provinces += 1
+                queue = collections.deque([i])
                 visited.add(i)
-                dfs(i)
+                while queue:
+                    city = queue.popleft()
+                    for neighbor, is_linked in enumerate(isConnected[city]):
+                        if is_linked and neighbor not in visited:
+                            visited.add(neighbor)
+                            queue.append(neighbor)
         
         return num_provinces
