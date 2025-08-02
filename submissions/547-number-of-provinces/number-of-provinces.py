b4bbdacccf9488ac4f52a6efe85ceb5980a9e2f9
Arindam Singh <118199077+SinghArindam@users.noreply.github.com>
2025-08-02 23:10:33 +0530

    Time: 4 ms (73.61%) | Memory: 19.8 MB (5.29%) - LeetSync

--- submissions/547-number-of-provinces/number-of-provinces.py
+++ submissions/547-number-of-provinces/number-of-provinces.py
@@ -1,19 +1,45 @@
-from typing import List
+# Approach 1
+# from typing import List
+
+# class Solution:
+#     def findCircleNum(self, isConnected: List[List[int]]) -> int:
+#         n = len(isConnected)
+#         visited = set()
+#         count = 0
+#         def dfs(node):
+#             visited.add(node)
+#             for i in range(n):
+#                 if isConnected[node][i] == 1 and i not in visited:
+#                     dfs(i)
+
+#         for i in range(n):
+#             if i not in visited:
+#                 count += 1
+#                 dfs(i)
+        
+#         return count
+
+
+
+# Approach 2
+import collections
 
 class Solution:
-    def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        n = len(isConnected)
+    def findCircleNum(self, isConnected: list[list[int]]) -> int:
+        num_cities = len(isConnected)
         visited = set()
-        count = 0
-        def dfs(node):
-            visited.add(node)
-            for i in range(n):
-                if isConnected[node][i] == 1 and i not in visited:
-                    dfs(i)
+        num_provinces = 0
 
-        for i in range(n):
+        def dfs(city_index):
+            for neighbor, is_linked in enumerate(isConnected[city_index]):
+                if is_linked and neighbor not in visited:
+                    visited.add(neighbor)
+                    dfs(neighbor)
+
+        for i in range(num_cities):
             if i not in visited:
-                count += 1
+                num_provinces += 1
+                visited.add(i)
                 dfs(i)
         
-        return count
+        return num_provinces
