9f8206d432f51017a052e2c8a26911732ed14aca
Arindam Singh <118199077+SinghArindam@users.noreply.github.com>
2025-08-02 23:12:51 +0530

    Time: 8 ms (35.42%) | Memory: 19.1 MB (58.75%) - LeetSync

--- submissions/547-number-of-provinces/number-of-provinces.py
+++ submissions/547-number-of-provinces/number-of-provinces.py
@@ -71,29 +71,59 @@
 
 
 # Approach 4
+# class Solution:
+#     def findCircleNum(self, isConnected: list[list[int]]) -> int:
+#         num_cities = len(isConnected)
+#         parent = list(range(num_cities))
+#         num_provinces = num_cities
+
+#         def find_root(i):
+#             if parent[i] == i:
+#                 return i
+#             parent[i] = find_root(parent[i])
+#             return parent[i]
+
+#         def union_sets(i, j):
+#             nonlocal num_provinces
+#             root_i = find_root(i)
+#             root_j = find_root(j)
+#             if root_i != root_j:
+#                 parent[root_j] = root_i
+#                 num_provinces -= 1
+
+#         for i in range(num_cities):
+#             for j in range(i + 1, num_cities):
+#                 if isConnected[i][j] == 1:
+#                     union_sets(i, j)
+        
+#         return num_provinces
+
+
+# Approach 5
+import collections
+
 class Solution:
     def findCircleNum(self, isConnected: list[list[int]]) -> int:
         num_cities = len(isConnected)
-        parent = list(range(num_cities))
-        num_provinces = num_cities
-
-        def find_root(i):
-            if parent[i] == i:
-                return i
-            parent[i] = find_root(parent[i])
-            return parent[i]
-
-        def union_sets(i, j):
-            nonlocal num_provinces
-            root_i = find_root(i)
-            root_j = find_root(j)
-            if root_i != root_j:
-                parent[root_j] = root_i
-                num_provinces -= 1
-
+        adj_list = collections.defaultdict(list)
         for i in range(num_cities):
             for j in range(i + 1, num_cities):
-                if isConnected[i][j] == 1:
-                    union_sets(i, j)
+                if isConnected[i][j]:
+                    adj_list[i].append(j)
+                    adj_list[j].append(i)
+
+        visited = set()
+        num_provinces = 0
+
+        def dfs(city_index):
+            visited.add(city_index)
+            for neighbor in adj_list[city_index]:
+                if neighbor not in visited:
+                    dfs(neighbor)
+
+        for i in range(num_cities):
+            if i not in visited:
+                num_provinces += 1
+                dfs(i)
         
         return num_provinces
