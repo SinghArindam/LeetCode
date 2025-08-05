37224472df756516f4b6c98de7b69b02df0f2e05
Arindam Singh <118199077+SinghArindam@users.noreply.github.com>
2025-08-05 23:40:12 +0530

    Time: 3 ms (20.20%) | Memory: 17.8 MB (69.96%) - LeetSync

--- submissions/2050-count-good-numbers/count-good-numbers.py
+++ submissions/2050-count-good-numbers/count-good-numbers.py
@@ -9,8 +9,23 @@ class Solution:
         # return (a * b) % mod
 
         # Approach 2
+        # mod = 10**9 + 7
+        # p = pow(20, n // 2, mod)
+        # if n % 2 == 1:
+        #     return (p * 5) % mod
+        # return p
+
+        # Approach 3
         mod = 10**9 + 7
-        p = pow(20, n // 2, mod)
-        if n % 2 == 1:
-            return (p * 5) % mod
-        return p
+        even = (n+1)//2
+        odd = n//2
+        def power(x, n, mod):
+            res = 1
+            while n>0:
+                if n%2==1:
+                    res = (res*x) % mod
+                x = (x*x) % mod
+                n = n//2
+            return res
+
+        return (power(5, even, mod) * power(4, odd, mod)) % mod
