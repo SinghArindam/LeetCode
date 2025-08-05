eb49c45ac2769b3cdb861bee29198df8cba0d4fc
Arindam Singh <118199077+SinghArindam@users.noreply.github.com>
2025-08-05 23:47:58 +0530

    Time: 3 ms (20.20%) | Memory: 18.4 MB (24.37%) - LeetSync

--- submissions/2050-count-good-numbers/count-good-numbers.py
+++ submissions/2050-count-good-numbers/count-good-numbers.py
@@ -27,20 +27,41 @@ class Solution:
         #         x = (x*x) % mod
         #         n = n//2
         #     return res
-
         # return (power(5, even, mod) * power(4, odd, mod)) % mod
 
         # Approach 4
-        mod = 10**9 + 7
-        even = (n+1)//2
-        odd = n//2
-        def power(x, n, mod):
-            if n == 0:
-                return 1
-            half = power(x, n//2, mod)
-            res = (half * half) % mod
-            if n%2==1:
-                res = (res*x) % mod
-            return res
+        # mod = 10**9 + 7
+        # even = (n+1)//2
+        # odd = n//2
+        # def power(x, n, mod):
+        #     if n == 0:
+        #         return 1
+        #     half = power(x, n//2, mod)
+        #     res = (half * half) % mod
+        #     if n%2==1:
+        #         res = (res*x) % mod
+        #     return res
+        # return (power(5, even, mod) * power(4, odd, mod)) % mod
 
-        return (power(5, even, mod) * power(4, odd, mod)) % mod
+        # Approach 5
+        # mod = 10**9 + 7
+        # odd = n >> 1
+        # even = n - odd
+        # a = pow(5, even, mod)
+        # b = pow(4, odd, mod)
+        # return (a * b) % mod
+
+        # Approach 6
+        mod = 10**9 + 7
+        memo = {}
+        def power(b, exp):
+            if (b, exp) in memo: return memo[(b, exp)]
+            if exp == 0: return 1
+            half = power(b, exp // 2)
+            res = (half * half) % mod
+            if exp % 2 == 1: res = (res * b) % mod
+            memo[(b, exp)] = res
+            return res
+        even = (n + 1) // 2
+        odd = n // 2
+        return (power(5, even) * power(4, odd)) % mod
